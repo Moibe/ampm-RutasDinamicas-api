defde4ddfe2de8bf4a43e3b7b0aecd8fb9135e52
Moibe <moi.estrello@gmail.com>
2024-04-23 13:26:38 -0600

    ajuste parámetros queries

--- queries.py
+++ queries.py
@@ -1,5 +1,5 @@
 #Definir el TOP desde aquí.
 #O puedo yo definirlo dinámicamente después de extraerlo de aquí.
-avanceTotal = "SELECT Total, Avance, Pendiente, [%Avance], [%Pendiente] FROM vwGetGuiasImpresasAvance where OficinaId=2 and TipoRuta='FIJA' ORDER BY Total DESC"
-avanceXCliente = "SELECT TOP (8) Cliente, Total, Avance, Pendiente, [%Avance], [%Pendiente] FROM vwGetGuiasImpresasXClienteAvance where OficinaId=2 and TipoRuta='FIJA' ORDER BY Total DESC"
-avanceXRuta = "SELECT TOP (5) Ruta, Total, Avance, Pendiente, [%Avance], [%Pendiente] FROM vwGetGuiasImpresasXRutaAvance where OficinaId=2 and TipoRuta='FIJA' ORDER BY Total DESC"
+avanceTotal = "SELECT Total, Avance, Pendiente, [%Avance], [%Pendiente] FROM vwGetGuiasImpresasAvance where OficinaId=2 and TipoRuta='DINAMICA' ORDER BY Total DESC"
+avanceXCliente = "SELECT TOP (5) Cliente, Total, Avance, Pendiente, [%Avance], [%Pendiente] FROM vwGetGuiasImpresasXClienteAvance where OficinaId=2 and TipoRuta='DINAMICA' ORDER BY Total DESC"
+avanceXRuta = "SELECT Ruta, Total, Avance, Pendiente, [%Avance], [%Pendiente] FROM vwGetGuiasImpresasXRutaAvance where OficinaId=2 and TipoRuta='DINAMICA' ORDER BY Total DESC"
