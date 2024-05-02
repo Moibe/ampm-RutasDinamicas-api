14f33795a52b16bc6bac3f8eaecc2f360539ce8f
Moibe <moi.estrello@gmail.com>
2024-05-02 12:33:46 -0600

    QUeries Ok

--- queries.py
+++ queries.py
@@ -1,5 +1,22 @@
 #Definir el TOP desde aquí.
-#O puedo yo definirlo dinámicamente después de extraerlo de aquí.
-avanceTotal = "SELECT Total, Avance, Pendiente, [%Avance], [%Pendiente] FROM vwGetGuiasImpresasAvance where OficinaId=2 and TipoRuta='DINAMICA' ORDER BY Total DESC"
-avanceXCliente = "SELECT TOP (5) Cliente, Total, Avance, Pendiente, [%Avance], [%Pendiente] FROM vwGetGuiasImpresasXClienteAvance where OficinaId=2 and TipoRuta='DINAMICA' ORDER BY Total DESC"
-avanceXRuta = "SELECT Ruta, Total, Avance, Pendiente, [%Avance], [%Pendiente] FROM vwGetGuiasImpresasXRutaAvance where OficinaId=2 and TipoRuta='DINAMICA' ORDER BY Total DESC"
+
+avanceTotal = """
+SELECT Total, Avance, Pendiente, [%Avance], [%Pendiente] 
+FROM vwGetGuiasImpresasAvance 
+WHERE OficinaId=2 and TipoRuta='DINAMICA' 
+ORDER BY Total DESC
+"""
+
+avanceXCliente = """
+SELECT LEFT(Cliente, 9) AS Cliente, Total, Avance, Pendiente, [%Avance], [%Pendiente] 
+FROM vwGetGuiasImpresasXClienteAvance 
+WHERE OficinaId=2 and TipoRuta='DINAMICA'
+ORDER BY Total ASC
+"""
+
+avanceXRuta = """
+SELECT Ruta, Total, Avance, Pendiente, [%Avance], [%Pendiente] 
+FROM vwGetGuiasImpresasXRutaAvance 
+WHERE OficinaId=2 and TipoRuta='DINAMICA' 
+ORDER BY Total DESC
+"""
